23a7c9efcadcf6f1fcb5c6c94a262f309cdeae3b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-03-24 23:38:28 +0100

    feat: update legal pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,52 @@
 import React from "react";
 import Link from "next/link";
 
+const LightLink = ({ href, children }) => (
+  <Link href={href} passHref>
+    <a
+      href={href}
+      style={{
+        color: "white",
+        fontSize: "0.8em",
+      }}
+    >
+      {children}
+    </a>
+  </Link>
+);
+
+const Separator = () => (
+  <span style={{ color: "white", marginRight: 5 }}>|</span>
+);
+
 export function Footer() {
   return (
     <footer
       style={{
         marginTop: 150,
         background: "var(--primary)",
-        color: "var(--white) !important",
+        color: "white",
       }}
     >
       <div className="container text-center">
-        <p>La Fabrique Numérique des Ministères Sociaux © 2020</p>
+        <p>
+          <a style={{ color: "white" }} href="https://fabrique.social.gouv.fr">
+            La Fabrique Numérique des Ministères Sociaux
+          </a>
+        </p>
         <ul className="list-inline">
           <li className="list-inline-item">
-            <Link href="/cgu" passHref>
-              <a
-                style={{
-                  color: "var(--white) !important",
-                  fontSize: "0.8em",
-                }}
-              >
-                condititions générales
-              </a>
-            </Link>
+            <LightLink href="/cgu">condititions générales</LightLink>
+          </li>
+          <Separator />
+          <li className="list-inline-item">
+            <LightLink href="/mentions-legales">mentions légales</LightLink>
+          </li>
+          <Separator />
+          <li className="list-inline-item">
+            <LightLink href="/politique-confidentialite">
+              politique de confidentialité
+            </LightLink>
           </li>
         </ul>
       </div>
