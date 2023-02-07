5466bb034ff5f7b220521a288e4e4fd6e05c2868
tangimds <tangimds@gmail.com>
2023-02-07 09:41:36 +0100

    chore: fix layout

--- components/footer.js
+++ components/footer.js
@@ -2,31 +2,29 @@ export default function Footer() {
   return (
     <footer className="container flex flex-col justify-between py-10 lg:py-20 lg:items-center lg:flex-row">
       <div className="mb-8 lg:mb-0">
-        <a className="inline-block mb-4 mr-5 lg:mb-10" href="">
-          <img className="w-10" src="images/logo.png" alt="" />
-        </a>
-
-        <a
-          className="inline-block mb-4 mr-5 lg:mb-10"
-          href="https://www.ars.sante.fr/"
-        >
-          <img className="w-11" src="images/logo_ars.png" alt="" />
-        </a>
-
-        <a
-          className="inline-block mb-4 mr-5 lg:mb-10"
-          href="http://www.eps-etampes.fr/en/accueil/"
-        >
-          <img className="w-15" src="images/logo_barth.png" alt="" />
-        </a>
-
-        <a
-          className="inline-block mb-4 mr-5 lg:mb-10"
-          href="https://solidarites-sante.gouv.fr/"
-        >
-          <img className="w-13" src="images/logo_fab.png" alt="" />
-        </a>
-
+        <div className="container flex space-x-4 mb-2">
+          <a className="inline-block mb-4 mr-5 lg:mb-10" href="">
+            <img className="w-10 h-10" src="images/logo.png" alt="" />
+          </a>
+          <a
+            className="inline-block mb-4 mr-5 lg:mb-10"
+            href="https://solidarites-sante.gouv.fr/"
+          >
+            <img className="h-10" src="images/logo_fab.png" alt="" />
+          </a>
+          <a
+            className="inline-block mb-4 mr-5 lg:mb-10"
+            href="https://www.ars.sante.fr/"
+          >
+            <img className="h-10" src="images/logo_ars.jpeg" alt="" />
+          </a>
+          <a
+            className="inline-block mb-4 mr-5 lg:mb-10"
+            href="http://www.eps-etampes.fr/en/accueil/"
+          >
+            <img className="h-10" src="images/logo_barth.jpeg" alt="" />
+          </a>
+        </div>
         <p className="mb-1 text-xs text-black opacity-70">
           Créé par le dr Lya Pedron, financé par l'ARS et la DINUM,
           <br />
